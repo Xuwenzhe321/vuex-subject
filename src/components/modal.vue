309a554a452b663521c7a0f205c3c84934aaee74
NDOER <ndobox@outlook.com>
2017-11-29 17:57:18 +0800

    遮罩层

---
+++ src/components/modal.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="modal">
+
+    </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<style>
+  #modal{
+    position: absolute;
+    top:0;
+    left:0;
+    width:100%;
+    height:100%;
+    background: rgba(0, 0, 0, 0.5);
+  }
+</style>
